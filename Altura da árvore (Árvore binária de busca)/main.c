c3dffc0586aff82704b5797c400324e81a6562be
José <gui.kcoal@hotmail.com>
2021-03-28 17:08:42 -0300

    added Altura da árvore (Árvore Binária de Busca)

--- Altura da árvore (Árvore binária de busca)/main.c
+++ Altura da árvore (Árvore binária de busca)/main.c
@@ -130,32 +130,99 @@ void Tree_insert(Tree *Tree, int key)
     }
 }
 
+// Algoritmo de busca para calcular a distancia entre um nó folha e a raiz da árvore
+int Tree_search(Tree *Tree, int key)
+{
+    int hight = 0;
+    Node *aux;
+
+    if(Tree != NULL)  
+    {
+        if(Tree->Tree_size <= 1) // se ativer apenas a raiz ou nenhum valor, a altura é zero
+            return 0;
+        else
+        {
+            if(key < Tree->Node->key) // se key está na esquerda da árvore
+            {
+                aux = Tree->Node->left;
+                hight++;
+
+                while(aux->key != key)
+                {
+                    if(key < aux->key) // esquerda
+                        aux = aux->left;
+                    else // direita
+                        aux = aux->right;
+                            
+                    hight++;
+                }
+            }
+            else // se key está na direita da árvore
+            {
+                aux = Tree->Node->right;
+                hight++;
+
+                while(aux->key != key)
+                {
+                    if(key < aux->key) // esquerda
+                        aux = aux->left;
+                    else // direita
+                        aux = aux->right;
+
+                    hight++;
+                }
+            }
+        }
+    }
+    return hight;
+}
+           
 // Algoritmo de percurso modificado para procurar nós-folha
-void posfix(Node *Node)
+void posfix(Node *Node, int *v, int *size) // retorna um vetor com os nós-folha e seu tamanho
 {
     if(Node != NULL)
     {
-        posfix(Node->left);
-        posfix(Node->right);
-        if(Node->left == NULL && Node->right == NULL) 
-            printf("%d ",Node->key);
+        posfix(Node->left, v, size);
+        posfix(Node->right, v, size);
+        if(Node->left == NULL && Node->right == NULL) // nó-folha
+        {
+            // como o vetor já tem espaço para 1 int, então é realocado apenas após a inserção do primeiro valor
+            if(*size > 0)
+                v = realloc(v, sizeof(v) + sizeof(int));
+            v[*size] = Node->key;
+            *size = *size + 1;
+        }
     }
 }
 
 int main() {
 
-    int Tree_size, i, key;
+    int Tree_size; // tamanho da árvore
+    int i; // auxiliar
+    int key; // valor a ser inserido na árvore
+    int *v = malloc(sizeof(int)); // vetor que armazena os nós-folha
+    int *size = malloc(sizeof(int)); // tamanho do vetor
+    int max_hight = 0; // altura máxima da árvore
     Tree *Tree = Tree_create(0);
 
     scanf("%d",&Tree_size);
 
+    // insere valores na árvore
     for(i = 0; i < Tree_size; i++)
     {
         scanf("%d",&key);
         Tree_insert(Tree, key);
     }
 
-    posfix(Tree->Node);
+    *size = 0;
+    posfix(Tree->Node, v, size);
+    
+    // calcular todas as alturas a partir dos nó-folha e retorna a maior
+    for(i = 0; i < *size; i++)
+        if(Tree_search(Tree, v[i]) > max_hight)
+            max_hight = Tree_search(Tree, v[i]);
+
+    printf("%d",max_hight);
 
     return 0;
 }
---
+++ Altura da árvore (Árvore binária de busca)/readme.txt
@@ -0,0 +1,15 @@
+Implemente uma função que receba uma árvore binária de busca. A função deverá retornar a altura da árvore.
+
+Input Format
+
+Na primeira linha deve ser lido o número inteiro N, que é referente à quantidade de nós da árvore binária de busca. Em seguida, em cada linha deve ser lido um número inteiro para ser inserido como nó na árvore (fazer isso até que N números sejam lidos).
+
+Constraints
+
+.
+
+Output Format
+
+O valor da altura da árvore.
+
+Lembre-se: a altura do nó raiz é zero.
